2658565fc12893346809195aa7faff9eb5a6d7c1
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2023-10-24 19:41:05 +0530

    added aws pipeline script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -5,25 +5,22 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html',
-    },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+	mode: 'development',
+	devServer: {
+		port: 8080,
+		historyApiFallback: {
+			index: 'index.html',
+		},
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
 };
 
 module.exports = merge(commonConfig, devConfig);
